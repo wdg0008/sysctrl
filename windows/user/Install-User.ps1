f9ae289db5253d7337d5aa74047e37a0bc2db593
William D. Goins V ("Quin") <116991920+wdg0008@users.noreply.github.com>
2026-08-28 17:56:43 -0500

    Updates to fix Install-User.ps1
    Adds image extensions and fixes MikTeX package ID
    Removes weird custom objects
    Specifies scope explicitly for winget source packages

--- windows/user/Install-User.ps1
+++ windows/user/Install-User.ps1
@@ -1,6 +1,6 @@
 <#
 .SYNOPSIS
-    This script installs all user-scope packages defined in the script.
+    This script installs a list of pre-defined, user-scope windows packages that it declares.
 .DESCRIPTION
     This script does not require administrative rights to work.
     It defines a list of package groups and their corresponding package IDs, and installs them using WinGet.
@@ -22,59 +22,49 @@ $wingetArguments = @(
     "--silent"
 )
 
-$packageGroups = @(
-    [pscustomobject]@{
-        Name = "Dev Tools"
-        PackageIds = @(
-            "Microsoft.PowerShell"
-            "Microsoft.PowerToys"
-            "Python.PythonInstallManager"
-        )
-    }
-    [pscustomobject]@{
-        Name = "Office and Productivity"
-        PackageIds = @(
-            "9WZDNCRFJ3PV"
-            "JGraph.Draw"
-            "MiKTex.MiKTeX"
-            "Obsidian.Obsidian"
-        )
-    }
-    [pscustomobject]@{
-        Name = "Utilities"
-        PackageIds = @(
-            "TexasInstruments.TIConnect"
-            "LocalSend.LocalSend"
-            "RickMeyers.e-Sword"
-            "Discord.Discord"
-        )
-    }
-    [pscustomobject]@{
-        Name = "Microsoft Store Apps"
-        PackageIds = @(
-            "9MSVH128X2ZT" # WinUI 2 Gallery
-            "9NBLGGH4TLCQ" # Windows Community Toolkit Sample App
-            "9NDLX60WX4KQ" # WPF Gallery
-            "9NKLCF1LVZ5H" # CommunityToolkit.MVVM Sample App
-            "9P3JFPWWDZRC" # WinUI 3 Gallery
-        )
-    }
+$winGetPackages = @(
+    # Dev Tools
+    "Microsoft.PowerShell"
+    "Python.PythonInstallManager"
+    # Office and Productivity
+    "JGraph.Draw"
+    "MiKTeX.MiKTeX"
+    "Obsidian.Obsidian"
+    # Utilities
+    "LocalSend.LocalSend"
+    "Discord.Discord"
 )
 
-foreach ($group in $packageGroups) {
-    $groupName = $group.Name
-    $packageIds = $group.PackageIds
+$storePackages = @(
+    # Regular Apps
+    "9MSVH128X2ZT" # WinUI 2 Gallery
+    "9NBLGGH4TLCQ" # Windows Community Toolkit Sample App
+    "9NDLX60WX4KQ" # WPF Gallery
+    "9NKLCF1LVZ5H" # CommunityToolkit.MVVM Sample App
+    "9P3JFPWWDZRC" # WinUI 3 Gallery
+    "9WZDNCRFJ3PV" # Windows Scan
+    "9N1F85V9T8BN" # Windows App (remote desktop)
+    "9MSMLRH6LZF3" # Windows Notepad
+    "9N0DX20HK701" # Windows Terminal
+    "9WZDNCRFHVN5" # Windows Calculator
+    # Image and Video Extensions
+    "9N95Q1ZZPMH4" # MPEG-2 Video Extension
+    "9MVZQVXJBQ9V" # AV1 Video Extension
+    "9NCTDW2W1BH8" # Raw Image Extension
+    "9MZPRTH5C0TB" # JPEG XL Image Extension
+    "9N5TDP8VCMHS" # Web Media Extensions
+    "9PB0TRCNRHFX" # AVC Encoder Video Extension
+    "9N4D0MSMP0PT" # VP9 Video Extensions
+    "9PMMSR1CGPWG" # HEIF Image Extension
+    "9PG2DK419DRG" # Webp Image Extension
+)
 
-    Write-Host "Installing $groupName...`n" -ForegroundColor Yellow
-
-    foreach ($packageId in $packageIds) {
-        Write-Host "Installing $packageId..."
-        winget @wingetArguments --id $packageId
-
-        if ($LASTEXITCODE -ne 0) {
-            throw "WinGet failed to install $packageId with exit code $LASTEXITCODE."
-        }
-    }
+foreach ($packageId in $winGetPackages) {
+    Write-Host "Installing $packageId..."
+    winget @wingetArguments --id $packageId --scope user --source winget
 }
 
-Write-Host "`nAll user-scope packages have been installed successfully.`n`n" -ForegroundColor Green
+foreach ($packageId in $storePackages) {
+    Write-Host "Installing $packageId from Microsoft Store..."
+    winget @wingetArguments --id $packageId --source msstore
+}
